735a55e41ade4c356f7342c86c90a68c32b52a36
Ranzascu Radu <radustefan3010@gmail.com>
2020-11-29 23:18:19 +0200

    Add project files.

---
+++ Data/Ranzascu_Ranzascu_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ranzascu_Ranzascu_Lab8.Models;
+
+namespace Ranzascu_Ranzascu_Lab8.Data
+{
+    public class Ranzascu_Ranzascu_Lab8Context : DbContext
+    {
+        public Ranzascu_Ranzascu_Lab8Context (DbContextOptions<Ranzascu_Ranzascu_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ranzascu_Ranzascu_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Ranzascu_Ranzascu_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Ranzascu_Ranzascu_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Migrations/20201123175600_IniatialCreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ranzascu_Ranzascu_Lab8.Migrations
+{
+    public partial class IniatialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Book",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Title",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Author",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
